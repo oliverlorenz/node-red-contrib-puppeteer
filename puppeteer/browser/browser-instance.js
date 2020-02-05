d72109400255b6d516d1da263fe24465b8d4ffab
sibeshkar <sibeshkar@users.noreply.github.com>
2020-02-05 19:28:42 +0530

    browser instance removed

--- puppeteer/browser/browser-instance.js
+++
@@ -1,9 +0,0 @@
-const puppeteer = require("puppeteer");
-
-module.exports = function(RED) {
-  function BrowserInstanceNode(n) {
-    RED.nodes.createNode(this, n);
-    this.instance = n.instance;
-  }
-  RED.nodes.registerType("browser-instance", BrowserInstanceNode);
-};
