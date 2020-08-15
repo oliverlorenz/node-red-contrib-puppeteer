f86c27956f827303602ad1704b55d6538c1c8fba
shubham-root <shubham.root@gmail.com>
2020-08-15 14:12:47 +0530

    fix for multiplatform

--- puppeteer/browser/browserutil/chrome.js
+++ puppeteer/browser/browserutil/chrome.js
@@ -1,8 +1,12 @@
 const open = require('open');
 const axios = require('axios');
 const os = require("os").platform;
+const homedir = os.homedir();
+browser_extract_mac = "chrome-mac/Chromium.app"
+browser_extract_linux = "chrome-linux/chrome"
+browser_extract_win = "chrome-win\\chrome.exe" 
+const browser = os === "darwin" ? homedir+"/.maya/"+browser_extract_mac : os === "win32" ? homedir+"\\.maya\\"+browser_extract_win: homedir+"/.maya/"+browser_extract_linux;
 
-const browser = os === "darwin"? process.env.MAYA_BROWSER_PATH+"Chromium":os === "win32"?"":process.env.MAYA_BROWSER_PATH+"chrome";
 const chrome = async () => {
   await open("", { app: [browser, '--remote-debugging-port=9222', '--no-first-run', '--no-default-browser-check'] });
   return await new Promise(r => setTimeout(r, 1000))
