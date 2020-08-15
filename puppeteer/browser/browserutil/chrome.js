90b1b83899481e8533b17edbd1b05bf292405212
shubham-root <shubham.root@gmail.com>
2020-08-15 14:37:41 +0530

    changed directory location

--- puppeteer/browser/browserutil/chrome.js
+++ puppeteer/browser/browserutil/chrome.js
@@ -5,7 +5,7 @@ const homedir = os.homedir();
 browser_extract_mac = "chrome-mac/Chromium.app"
 browser_extract_linux = "chrome-linux/chrome"
 browser_extract_win = "chrome-win\\chrome.exe" 
-const browser = os === "darwin" ? homedir+"/.maya/"+browser_extract_mac : os === "win32" ? homedir+"\\.maya\\"+browser_extract_win: homedir+"/.maya/"+browser_extract_linux;
+const browser = os === "darwin" ? homedir+"/.maya/bin/"+browser_extract_mac : os === "win32" ? homedir+"\\.maya\\bin\\"+browser_extract_win: homedir+"/.maya/bin/"+browser_extract_linux;
 
 const chrome = async () => {
   await open("", { app: [browser, '--remote-debugging-port=9222', '--no-first-run', '--no-default-browser-check'] });
