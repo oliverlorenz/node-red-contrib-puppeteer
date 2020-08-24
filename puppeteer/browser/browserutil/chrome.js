fa6b1d98b11976e5dc9e23082a510cee7bf450bf
shubham-root <shubham.root@gmail.com>
2020-08-24 15:28:44 +0530

    multiselect

--- puppeteer/browser/browserutil/chrome.js
+++ puppeteer/browser/browserutil/chrome.js
@@ -1,11 +1,12 @@
 const open = require('open');
 const axios = require('axios');
-const os = require("os").platform;
+const os = require("os");
+const platform = os.platform();
 const homedir = os.homedir();
 browser_extract_mac = "chrome-mac/Chromium.app"
 browser_extract_linux = "chrome-linux/chrome"
 browser_extract_win = "chrome-win\\chrome.exe" 
-const browser = os === "darwin" ? homedir+"/.maya/bin/"+browser_extract_mac : os === "win32" ? homedir+"\\.maya\\bin\\"+browser_extract_win: homedir+"/.maya/bin/"+browser_extract_linux;
+const browser = platform === "darwin" ? homedir+"/.maya/bin/"+browser_extract_mac : platform === "win32" ? homedir+"\\.maya\\bin\\"+browser_extract_win: homedir+"/.maya/bin/"+browser_extract_linux;
 
 const chrome = async () => {
   await open("", { app: [browser, '--remote-debugging-port=9222', '--no-first-run', '--no-default-browser-check'] });
--- puppeteer/pageutils/arrayselect.js
+++ puppeteer/pageutils/arrayselect.js
@@ -3,6 +3,8 @@
 // let elem1 = "#search > div.s-desktop-width-max.s-opposite-dir > div > div.sg-col-20-of-24.s-matching-dir.sg-col-28-of-32.sg-col-16-of-20.sg-col.sg-col-32-of-36.sg-col-8-of-12.sg-col-12-of-16.sg-col-24-of-28 > div > span:nth-child(4) > div.s-main-slot.s-result-list.s-search-results.sg-row > div:nth-child(2) > div > span > div > div > div:nth-child(2) > div.sg-col-4-of-12.sg-col-8-of-16.sg-col-16-of-24.sg-col-12-of-20.sg-col-24-of-32.sg-col.sg-col-28-of-36.sg-col-20-of-28 > div > div:nth-child(1) > div > div > div:nth-child(1) > h2 > a > span"
 // let elem2 = "#search > div.s-desktop-width-max.s-opposite-dir > div > div.sg-col-20-of-24.s-matching-dir.sg-col-28-of-32.sg-col-16-of-20.sg-col.sg-col-32-of-36.sg-col-8-of-12.sg-col-12-of-16.sg-col-24-of-28 > div > span:nth-child(4) > div.s-main-slot.s-result-list.s-search-results.sg-row > div:nth-child(3) > div > span > div > div > div:nth-child(2) > div.sg-col-4-of-12.sg-col-8-of-16.sg-col-16-of-24.sg-col-12-of-20.sg-col-24-of-32.sg-col.sg-col-28-of-36.sg-col-20-of-28 > div > div:nth-child(1) > div > div > div:nth-child(1) > h2 > a > span"
 
+// let elem1 = "#row-0 > div > div > div > div > div > div.slider-item.slider-item-1"
+// let elem2 = "#row-0 > div > div > div > div > div > div.slider-item.slider-item-2"
   function getSelector(elem1, elem2) {
     var shorterLength = Math.min(elem1.length, elem2.length);
     var nPos;
